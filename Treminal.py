9c4474257e7288adf6758d3170ff236cf8ec5fbe
Johnny Guimarães <johnnyreisudi@gmail.com>
2023-09-13 15:28:50 -0300

    0.0.3

--- Treminal.py
+++ Treminal.py
@@ -1,14 +1,15 @@
-import basic
+import mineiro
 
 while True:
-	text = input('Queijo > ')
-	if text.strip() == "": continue
-	result, error = basic.run('<stdin>', text)
+    text = input('Queijo > ')
+    if text.strip() == "":
+        continue
+    result, error = mineiro.run('<stdin>', text)
 
-	if error:
-		print(error.as_string())
-	elif result:
-		if len(result.elements) == 1:
-			print(repr(result.elements[0]))
-		else:
-			print(repr(result))
+    if error:
+        print(error.as_string())
+    elif result:
+        if len(result.elements) == 1:
+            print(repr(result.elements[0]))
+        else:
+            print(repr(result))
